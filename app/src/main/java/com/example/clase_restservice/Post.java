9f4cd5be9b27c1d53b56964f91d39ec3cab92a35
Hamilton <Hamilton-Novoa@hotmail.com>
2020-10-23 15:23:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/clase_restservice/Post.java
@@ -0,0 +1,34 @@
+package com.example.clase_restservice;
+
+public class Post {
+    private int userId;
+    private int id;
+    private String tittle;
+    private String body;
+
+    public Post() {
+
+    }
+
+    public int getUserId() {
+        return userId;
+
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return tittle;
+
+
+    }
+}
+
+
+
